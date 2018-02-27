c145994dff83052c7a47446c468b4a8c3c354144
Giovanny Betancourt G <geobg711@hotmail.com>
2018-02-26 19:04:08 -0500

    C2 - master - Segundo artículo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { TransferState, makeStateKey } from '@angular/platform-browser';
+import 'rxjs/add/operator/map';
+
+const PEOPLE_KEY = makeStateKey('people');
+
+interface User {
+
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  public people: User[] = [];
+
+  constructor (
+    private http: HttpClient,
+    private state: TransferState
+  ) { }
+
+  ngOnInit() {
+    this.people = this.state.get<User[]>(PEOPLE_KEY, null);
+    if (!this.people) {
+      this.http.get<any>('https://randomuser.me/api/?results=10&seed=foobar')
+      .map(data => <User[]>data.results)
+      .subscribe(data => {
+        console.log('data retrieved from API');
+        this.people = data;
+        this.state.set<User[]>(PEOPLE_KEY, data);
+       });
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
+import { NgModule, PLATFORM_ID, APP_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 
 import { AppComponent } from './app.component';
@@ -10,9 +11,18 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({appId: 'universal-project1'}),
+    BrowserTransferStateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: object,
+    @Inject(APP_ID) private appId: string
+  ) {
+    const platform = isPlatformBrowser(this.platformId) ? 'browser' : 'server';
+    console.log('I\'m on the ', platform);
+  }
+ }
